fd874c159b7152b74852a00268005f0be8c6e335
ANISHCAPRICORN <anishelectronics10@gmail.com>
2018-06-25 23:45:17 +0530

    Modified Source Code. Included Author Name Entry Output file Format Modified Included Exe File also

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ Date    :   23/06/2018
 '''
 
 import time
-import re
+import re, sys
 from Tkinter import *
 import Tkinter, Tkconstants, tkFileDialog, tkMessageBox
 
 
 Str = '''*********************************************************
-*   Program         : Doxygen Comments Generator          
-*   Version         : 1.1                               
-*   Developer       : Anish Kumar                       
-*   Date And Time   : {}                                          
+*   Program      :   Doxygen Comments Generator          
+*   Version      :   1.1                               
+*   Developer    :   Anish Kumar                       
+*   Date         :   25-June-2018                                           
 *********************************************************\
-'''.format(time.asctime())
+'''
 COU_TEST_Count = 0
 COU_TEST_Flag, COU_CALL_Flag, Assert_Print_Flag, Event_Print_Flag = 0, 0, 0, 0
 ASSERT_Missing = 0
@@ -27,20 +27,19 @@ Events = []
 Order_Check_Flag = 0  #
 COU_SET_Count, COU_CALL_Count, COU_ASSERT_Count, COU_TEST_Count = 0, 0, 0, 0
 Num_Lines = 0
-
+Name = " "
 
 def event_updater():
     global Event_Print_Flag
     global Events
     if Event_Print_Flag:
-        dest.write("\n@events\n")
+        dest.write("*\n* @events\n*")
         Event_Print_Flag = 0
         Events = set(Events)
         for i in Events:
-            print(i)
             dest.write('\t')
             dest.write(i)
-            dest.write('\n')
+            dest.write('\n*')
         Events = []
 
 
@@ -48,23 +47,22 @@ def results_updater():
     global Assert_Print_Flag
     global Asserts
     if Assert_Print_Flag:
-        dest.write("\n@results\n")
+        dest.write("\n* @results\n**\t")
         Assert_Print_Flag = 0
-        Asserts = set(Asserts)
         for i in Asserts:
-            print(i)
-            if i != '\n*':
-                dest.write('\t')
             dest.write(i)
-            if i != '\n*':
-                dest.write('\n')
+            dest.write('\t')
         Asserts = []
 
 
 def generator():
     global COU_TEST_Count, ASSERT_Missing, Missed_Asserts, Order_Check_Flag, TEST_CASE_Name, COU_ASSERT_Count
     global dest, COU_CALL_Flag, COU_TEST_Flag, COU_SET_Count, COU_CALL_Count, Event_Print_Flag, Assert_Print_Flag
-    global Num_Lines
+    global Num_Lines, Name
+    global Name, E1
+    First_Time = 0
+    Name = E1.get()
+
     # Loop Starts here
     with open(filepath) as fp:
         # Reading Line by Line
@@ -89,15 +87,23 @@ def generator():
                 event_updater()
 
                 results_updater()
-
+                if First_Time:
+                    dest.write("\n* @type\n*\n* @regression\n*\n* @integration\n*\n* @validates\n*\n*")
+                First_Time = 1
                 dat = (ch.strip())
                 TEST_CASE_Name = (re.search('"(.+?)"', dat)).group(1)
                 if Order_Check_Flag == 0:  # Order_Check_Flag = zero  Means Executing after COU_ASSERT
-                    dest.write("\n\n\n")
+                    dest.write("\n**/\n\n\n ")
                     dest.write(TEST_CASE_Name)
                     dest.write("\n============================================================\n")
                     Order_Check_Flag = 1
-                    dest.write("@preconditions\n")
+                    dest.write("/**\n")
+                    dest.write("* @brief\n*\n*")
+                    dest.write(" @description\n*\n*")
+                    dest.write(" @author\n*\t")
+                    dest.write(Name)
+                    dest.write("\n*\n")
+                    dest.write("* @preconditions\n*")
                 break
             else:
                 pass
@@ -113,14 +119,17 @@ def generator():
                     Missed_Asserts.append(TEST_CASE_Name)  # Assert missing finding logic
                 COU_SET_Count += 1
                 if Order_Check_Flag == 0 and COU_TEST_Flag:
-                    dest.write("\n*")
+                    dest.write("*\n*")
                     Order_Check_Flag = 1
-                    Asserts.append('\n*')
+                    Asserts.append('\n**')
                 dat = (ch.strip())
                 dat1 = re.search('"(.+?)"', dat)
-                if COU_TEST_Flag:
-                    found = dat1.group(1)
-                    dest.write('\t')
+                if COU_TEST_Flag:                   # Omiting checking SET before starting Test cases
+                    if dat1:
+                        found = dat1.group(1)
+                    else:
+                        pass
+                    dest.write('*\t')
                     dest.write(found)
                     dest.write('\n')
                     ''' 
@@ -135,7 +144,10 @@ def generator():
                 Event_Print_Flag = 1
                 dat = (ch.strip())
                 dat1 = re.search('"(.+?)"', dat)
-                found = dat1.group(1)
+                if dat1:
+                    found = dat1.group(1)
+                else:
+                    pass
                 Events.append(found)
                 ''' 
                 ==============================================================================
@@ -149,8 +161,14 @@ def generator():
 
                 dat = (ch.strip())
                 dat1 = re.search('"(.+?)"', dat)
-                found = dat1.group(1)
-                Asserts.append(found)
+                current_assert_list = set(Asserts)
+                if dat1:
+                    found = dat1.group(1)
+                else:
+                    pass
+                if found not in current_assert_list:
+                    Asserts.append(found)
+                    Asserts.append('\n*')
                 Assert_Print_Flag = 1
             else:
                 pass
@@ -175,6 +193,7 @@ def generator():
 ========================================================
 *                   SUMMARY                            *        
 ========================================================
+*   Date and Time               : {}
 *   Input File                  : {}                                
 *   Output File                 : {}
 *   Number Of Test Cases        : {}
@@ -182,31 +201,36 @@ def generator():
 *   Number Of Asserts           : {}
 *   Number Of Asserts Missed    : {}
 *   Where You Missed            :  {}
-======================================================='''.format(fp.name, dest.name, COU_TEST_Count, Num_Lines,
+======================================================='''.format(time.asctime(), fp.name, dest.name, COU_TEST_Count, Num_Lines,
                                                                   COU_ASSERT_Count, ASSERT_Missing,
                                                                   "\n\t\t\t\t\t\t\t\t\t".join(Missed_Asserts))
 
     dest.close()
-    dest = open("Doxygen_Gen.txt", "r")
+    dest = open("output/Doxygen_Gen.txt", "r")
     contents = dest.readlines()
     dest.close()
     contents.insert(6, Str1)
-    dest = open("Doxygen_Gen.txt", "w")
+    dest = open("output/Doxygen_Gen.txt", "w")
     contents = "".join(contents)
     dest.write(contents)
     fp.close()
-    T = Text(root, height=15, width=300)
-    T.pack()
-    T.insert(END, Str1)
+    dest.close()
+    T = Text(root, height=2, width=30)
+    T.pack(pady = 20)
+    T.insert(END, "Generation Finished !\nPlease close this window")
 
+
+# def quit():
+#     sys.exit()
+#
 
 def gui_main():
-    global root, FileChoose
+    global root, FileChoose, Name, E1
     root = Tk()
     root.title('Doxygen Generator')
-
-    w = 800  # width for the Tk root
-    h = 600 # height for the Tk root
+    root.resizable(0, 0)
+    w = 500  # width for the Tk root
+    h = 100 # height for the Tk root
 
     # get screen width and height
     ws = root.winfo_screenwidth()  # width of the screen
@@ -215,24 +239,45 @@ def gui_main():
     # calculate x and y coordinates for the Tk root window
     x = (ws / 2) - (w / 2)
     y = (hs / 2) - (h / 2)
-
     # set the dimensions of the screen
     # and where it is placed
     root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
     frame = Frame(root)
     frame.pack()
-    button = Button(frame,
-                    text="QUIT",
-                    fg="red",
-                    command=quit)
-    button.pack(side=LEFT)
+
+
+    ''' 
+    ==============================================================================
+                        CHOOSE FILE BUTTON
+    ==============================================================================
+    '''
     FileChoose = Button(frame,
                         text="Choose File",
                         command=choose_file)
-    FileChoose.pack(side=LEFT)
+    FileChoose.pack(side=LEFT, padx = 20)
 
+    ''' 
+    ==============================================================================
+                        INFO BUTTON
+    ==============================================================================
+    '''
+    button_info = Button(frame,
+                    text="Info",
+                    fg="blue",
+                    command=info)
+    button_info.pack(side=RIGHT)
+
+    L1 = Label(root, text="Author Name")
+    L1.pack(side=LEFT, fill = X, padx = 20, pady = 0)
+
+    E1 = Entry(root, bd=5)
+    E1.pack(side=LEFT)
     root.mainloop()
+
+
+def info():
+    tkMessageBox.showinfo("Doxygen Generator", Str)
 
 
 def choose_file():
@@ -245,6 +290,6 @@ def choose_file():
 
 
 if __name__ == '__main__':
-    dest = open("Doxygen_Gen.txt", "w")
+    dest = open("output/Doxygen_Gen.txt", "w")
     gui_main()
-    dest.close()
+
